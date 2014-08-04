edc2b391cec62ee8947797eaf7e6bd6d326be9fd
Onur EKER <onur.eker@gmail.com>
2014-08-04 13:53:18 +0300

    * RichTextBoxWriter.cs is added

---
+++ Source/Projects/Bosphorus.Common.Clr/IO.TextWriter/RichTextBoxWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bosphorus.Common.Clr.IO.TextWriter
+{
+    public class RichTextBoxWriter : System.IO.TextWriter
+    {
+        private readonly RichTextBox output;
+
+        public RichTextBoxWriter(RichTextBox output)
+        {
+            this.output = output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            string text = value.ToString(CultureInfo.InvariantCulture);
+            output.AppendText(text);
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+}
