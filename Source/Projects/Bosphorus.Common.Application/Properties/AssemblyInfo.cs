94dd5b47d831a5a675f212114c509687741d8ccf
Onur EKER <onur.eker@gmail.com>
2016-02-29 23:48:55 +0200

    AssemblyInfo fix for nuget
    AssemblyInfo fix for nuget

--- Source/Projects/Bosphorus.Common.Application/Properties/AssemblyInfo.cs
+++ Source/Projects/Bosphorus.Common.Application/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bosphorus.Common.Application")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bosphorus.Common.Application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bosphorus")]
 [assembly: AssemblyProduct("Bosphorus.Common.Application")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
