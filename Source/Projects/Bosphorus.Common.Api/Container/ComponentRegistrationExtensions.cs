17c2ed238ab55dffa654d4a0f6431cccff35dfd3
Onur EKER <onur.eker@gmail.com>
2016-03-02 23:41:13 +0200

    Registration extension is added
    Registration extension is added

---
+++ Source/Projects/Bosphorus.Common.Api/Container/ComponentRegistrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+
+namespace Castle.MicroKernel.Registration
+{
+    public static class ComponentRegistrationExtensions
+    {
+        public static ComponentRegistration<TService> NamedFull<TService>(this ComponentRegistration<TService> extended)
+            where TService : class
+        {
+            Type serviceType = typeof (TService);
+            Type implementationType = extended.Implementation;
+            string name = $"{serviceType} - {implementationType}";
+            extended.Named(name);
+            return extended;
+        }
+
+    }
+}
