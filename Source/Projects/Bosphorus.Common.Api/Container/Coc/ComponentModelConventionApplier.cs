a87d1da9e728ba53294c00af78e96cc20500a12c
Onur EKER <onur.eker@gmail.com>
2016-11-12 13:49:04 +0300

    Container CoC fix

--- Source/Projects/Bosphorus.Common.Api/Container/Coc/ComponentModelConventionApplier.cs
+++ Source/Projects/Bosphorus.Common.Api/Container/Coc/ComponentModelConventionApplier.cs
@@ -4,20 +4,29 @@ using Bosphorus.Common.Api.CoC.Convention;
 using Castle.Core;
 using Castle.MicroKernel;
 using Castle.MicroKernel.ModelBuilder;
+using Castle.Windsor;
 
 namespace Bosphorus.Common.Api.Container.Coc
 {
     public class ComponentModelConventionApplier: IContributeComponentModelConstruction
     {
         private readonly Lazy<IConventionApplier<ComponentModelAcceptance>> conventionApplier;
+        private readonly IWindsorContainer container;
 
-        public ComponentModelConventionApplier(Lazy<IConventionApplier<ComponentModelAcceptance>> conventionApplier)
+        public ComponentModelConventionApplier(Lazy<IConventionApplier<ComponentModelAcceptance>> conventionApplier, IWindsorContainer container)
         {
             this.conventionApplier = conventionApplier;
+            this.container = container;
         }
 
         public void ProcessModel(IKernel kernel, ComponentModel model)
         {
+            //HACK: If convention applier is not registered yet, so go on..
+            if (!container.Kernel.HasComponent(typeof(IConventionApplier<>)))
+            {
+                return;
+            }
+
             if (model.Services.Any(type => type == typeof(IConventionApplier<ComponentModelAcceptance>)))
             {
                 return;
